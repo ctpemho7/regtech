2b4192f7e98d949255e0b0d05b242bb5e1f18cc7
Semyon <lukinsemyon@yandex.ru>
2025-11-12 22:41:40 +0300

    fix containerdoc

--- xml_declaration/wrapper_classes.py
+++ xml_declaration/wrapper_classes.py
@@ -1,39 +1,25 @@
 import xml.etree.ElementTree as ET
-from xml.dom import minidom
-from typing import TypeVar
-from abc import ABC, abstractmethod
+
+from base import IXMLElement
+
+from ESADout_CU import ESADout_CU
+from DTSout_CU import DTSout_CU
+from GTDoutCustomsMark import GTDoutCustomsMark
 
 
-class IXMLElement(ABC):
-    """Общий интерфейс для описания всех XML элементов"""
-    
-    @abstractmethod
+class Container(IXMLElement):
+    """Класс для описания Container"""
+    def __init__(self, content: IXMLElement = None):
+        self.content = content
+
     def to_xml(self) -> ET.Element:
-        pass
+        """Преобразовать объект в XML элемент"""
+        doc_body_elem = ET.Element('ContainerDoc')
+        if self.content:
+            content_elem = self.content.to_xml()
+            doc_body_elem.append(content_elem)
+        return doc_body_elem
 
-def pretty_print_xml_minidom(element: IXMLElement) -> None:
-    """
-    Красиво выводит XML элемент используя xml.dom.minidom
-    
-    Args:
-        element: Элемент, реализующий интерфейс IXMLElement
-    """
-    import xml.dom.minidom
-    
-    xml_elem = element.to_xml()
-    
-    # Конвертируем Element в строку и парсим через minidom
-    rough_string = ET.tostring(xml_elem, 'utf-8')
-    reparsed = xml.dom.minidom.parseString(rough_string)
-    pretty_xml = reparsed.toprettyxml(indent="  ")
-    
-    # Убираем лишние пустые строки
-    lines = pretty_xml.split('\n')
-    non_empty_lines = [line for line in lines if line.strip()]
-    print('\n'.join(non_empty_lines))
-
-
-XMLElementType = TypeVar('XMLElementType', bound=IXMLElement)
 
 
 class DocBody(IXMLElement):
@@ -55,9 +41,17 @@ class EDContainer(IXMLElement):
         self.document_mode_id = document_mode_id
         self.document_id = document_id
         # согласно XML у нас три элемента
-        self.ESADout_CU: DocBody = None
-        self.DTSout_CU: DocBody = None
-        self.GTDoutCustomsMark: DocBody = None
+        self.ESADout_CU: ESADout_CU = None
+        self.DTSout_CU: DTSout_CU = None
+        self.GTDoutCustomsMark: GTDoutCustomsMark = None
+
+    def containarize(self, doc: IXMLElement):
+        """ Положить в контейнер """
+        return Container(
+            DocBody(
+                doc
+            )
+        )
 
     def to_xml(self) -> ET.Element:
         """Преобразовать объект в XML элемент"""
@@ -73,15 +67,15 @@ class EDContainer(IXMLElement):
         
         # Добавляем дочерние элементы если они заданы
         if self.ESADout_CU:
-            esad_elem = self.ESADout_CU.to_xml()
+            esad_elem = self.containarize(self.ESADout_CU).to_xml()
             root.append(esad_elem)
             
         if self.DTSout_CU:
-            dts_elem = self.DTSout_CU.to_xml()
+            dts_elem = self.containarize(self.DTSout_CU).to_xml()
             root.append(dts_elem)
             
         if self.GTDoutCustomsMark:
-            gtd_elem = self.GTDoutCustomsMark.to_xml()
+            gtd_elem = self.containarize(self.GTDoutCustomsMark).to_xml()
             root.append(gtd_elem)
             
         return root
